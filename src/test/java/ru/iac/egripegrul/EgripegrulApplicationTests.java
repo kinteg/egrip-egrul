94b2596f1c190679c06719403fa20e84fdde5898
kinteg <zeroxchao@gmail.com>
2020-01-21 23:20:46 +0300

    Create project

---
+++ src/test/java/ru/iac/egripegrul/EgripegrulApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.iac.egripegrul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgripegrulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
